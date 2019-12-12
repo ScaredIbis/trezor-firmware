8c78fa8acb687fffd5b5610ac45992826936f881
Thomas White <im.toms.inbox@gmail.com>
2019-12-12 17:07:27 +1000

    Updated MosiacSupplyChange
    Added layout, updated tests.

--- python/src/trezorlib/messages/__init__.py
+++ python/src/trezorlib/messages/__init__.py
@@ -288,6 +288,7 @@ from . import LiskTransactionType
 from . import MessageType
 from . import NEM2AliasAction
 from . import NEM2EntityType
+from . import NEM2MosaicSupplyChangeAction
 from . import NEM2NamespaceRegistrationType
 from . import NEM2NetworkType
 from . import NEMImportanceTransferMode
